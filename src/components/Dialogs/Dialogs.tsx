b5658db638731e9bc23d51eaeefe4f03f07c1345
Anton <antonrozdobudko@gmail.com>
2021-08-03 14:35:34 +0300

    21 lesson. Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,38 @@ import d from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={d.allPageDialogs}>
+            <div className={d.dialogs}>
+                <div className={`${d.dialogItem} ${d.active}`}>
+                    Dimych
+                </div>
+                <div className={d.dialogItem}>
+                    Andrey
+                </div>
+                <div className={d.dialogItem}>
+                    Sveta
+                </div>
+                <div className={d.dialogItem}>
+                    Sasha
+                </div>
+                <div className={d.dialogItem}>
+                    Viktor
+                </div>
+                <div className={d.dialogItem}>
+                    Valera
+                </div>
+            </div>
+            <div className={d.messages}>
+                <div className={d.messageItem}>
+                    Hi
+                </div>
+                <div className={d.messageItem}>
+                    How is your it-kamasutra?
+                </div>
+                <div className={d.messageItem}>
+                    Yo
+                </div>
+            </div>
         </div>
     )
 }
