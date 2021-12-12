c43e2e9fa9efe392d7855e79a48389c8a7c7297f
Anton <antonrozdobudko@gmail.com>
2021-12-12 15:43:29 +1000

    51 lesson. Users API. Axios.

--- src/Redux/users_reducer.tsx
+++ src/Redux/users_reducer.tsx
@@ -1,16 +1,16 @@
 export type UsersPropsType = {
-    id: number
-    follow: boolean
     name: string
-    image: string
-    location: {
-        country: string
-        city: string
+    id: number
+    uniqueUrlName: string | null
+    photos: {
+        small: string | null
+        large: string | null
     }
-    status: string
+    status: string | null
+    followed: boolean
 }
 
-const initialUsersState = {
+/*const initialUsersState = {
     users: [
         {
             id: 1,
@@ -37,20 +37,23 @@ const initialUsersState = {
             status: 'Life is good!'
         }
     ] as UsersPropsType[]
+}*/
+const initialUsersState = {
+    items: [] as UsersPropsType[]
 }
 
 export type UsersPagePropsType = typeof initialUsersState
 
 export const followAC = (userID: number) => ({type: "FOLLOW", userID: userID} as const)
-export const setUsersAC = (users: UsersPagePropsType) => ({type: "SET-USERS", users: users} as const)
+export const setUsersAC = (items: UsersPagePropsType) => ({type: "SET-USERS", items: items} as const)
 export type ActionUsersPropsType = ReturnType<typeof followAC> | ReturnType<typeof setUsersAC>
 
 export const usersReducer = (state: UsersPagePropsType = initialUsersState, action: ActionUsersPropsType): UsersPagePropsType => {
     switch (action.type) {
         case "FOLLOW":
-            return {...state, users: state.users.map(m => m.id === action.userID ? {...m, follow: !m.follow} : m)}
+            return {...state, items: state.items.map(m => m.id === action.userID ? {...m, followed: !m.followed} : m)}
         case "SET-USERS":
-            return {...state, ...action.users}
+            return {...state, ...action.items}
         default:
             return state
     }
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {UsersPropsType} from './UsersContainer';
 import styles from './Users.module.css'
+import axios, {AxiosResponse} from 'axios';
+import userPhoto from '../../Assets/Images/user.jpg'
 
 export const Users = (props: UsersPropsType) => {
+    if (props.usersPage.items.length === 0) {
+        axios
+            .get<any>('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response: AxiosResponse) => props.setUsersHandler(response.data))
+    }
+
     return (
         <div className={styles.main}>
-            {props.usersPage.users.map(m =>
+            {props.usersPage.items.map(m =>
                 <div className={styles.usersBlocks}>
                     <div className={styles.imageBlock}>
-                        <img src={m.image} className={styles.image} alt={"User avatar"}/>
-                        <button onClick={() => props.followClick(m.id)}>{m.follow ? "Follow" : "Unfollow"}</button>
+                        <img src={m.photos.small !== null ? m.photos.small : userPhoto} className={styles.image}
+                             alt={'User avatar'}/>
+                        <button onClick={() => props.followClick(m.id)}>{m.followed ? 'Follow' : 'Unfollow'}</button>
                     </div>
                     <div className={styles.rightField}>
                         <div className={styles.upField}>
                             <div>{m.name}</div>
                             <div>
-                                <div>{`${m.location.country},`}</div>
-                                <div>{m.location.city}</div>
+                                <div>Russia,</div>
+                                <div>Moscow</div>
                             </div>
                         </div>
                         <div className={styles.status}>{m.status}</div>
