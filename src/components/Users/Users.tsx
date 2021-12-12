5eb9df949c4b81217edd170899eb1c9c30f03ad4
Anton <antonrozdobudko@gmail.com>
2021-12-12 18:52:01 +1000

    53 lesson. Creating Class Component for Users. Extends React.Component.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,39 +3,44 @@ import {UsersPropsType} from './UsersContainer';
 import styles from './Users.module.css'
 import axios, {AxiosResponse} from 'axios';
 import userPhoto from '../../Assets/Images/user.jpg'
+import {UsersPagePropsType} from '../../Redux/users_reducer';
 
-export const Users = (props: UsersPropsType) => {
-    const getUsers = () => {
+
+// Здесь используем классовый компонент, смотреть урок 53
+export class Users extends React.Component<UsersPropsType, UsersPagePropsType> {
+    constructor(props: UsersPropsType) {
+        super(props);
         axios
             .get<any>('https://social-network.samuraijs.com/api/1.0/users')
-            .then((response: AxiosResponse) => props.setUsersHandler(response.data))
+            .then((response: AxiosResponse) => this.props.setUsersHandler(response.data))
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>Get users</button>
-            <div className={styles.main}>
-                {props.usersPage.items.map(m =>
-                    <div className={styles.usersBlocks}>
-                        <div className={styles.imageBlock}>
-                            <img src={m.photos.small !== null ? m.photos.small : userPhoto} className={styles.image}
-                                 alt={'User avatar'}/>
-                            <button
-                                onClick={() => props.followClick(m.id)}>{m.followed ? 'Follow' : 'Unfollow'}</button>
-                        </div>
-                        <div className={styles.rightField}>
-                            <div className={styles.upField}>
-                                <div>{m.name}</div>
-                                <div>
-                                    <div>Russia,</div>
-                                    <div>Moscow</div>
-                                </div>
+    render() {
+        return (
+            <div>
+                <div className={styles.main}>
+                    {this.props.usersPage.items.map(m =>
+                        <div className={styles.usersBlocks}>
+                            <div className={styles.imageBlock}>
+                                <img src={m.photos.small !== null ? m.photos.small : userPhoto} className={styles.image}
+                                     alt={'User avatar'}/>
+                                <button
+                                    onClick={() => this.props.followClick(m.id)}>{m.followed ? 'Follow' : 'Unfollow'}</button>
                             </div>
-                            <div className={styles.status}>{m.status}</div>
+                            <div className={styles.rightField}>
+                                <div className={styles.upField}>
+                                    <div>{m.name}</div>
+                                    <div>
+                                        <div>Russia,</div>
+                                        <div>Moscow</div>
+                                    </div>
+                                </div>
+                                <div className={styles.status}>{m.status}</div>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
